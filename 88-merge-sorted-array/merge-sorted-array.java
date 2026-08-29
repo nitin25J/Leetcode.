279bc1358eca07e80c32c31635d0a75d32ec13a0
nitin25J <nitin.25bai11122@vitbhopal.ac.in>
2026-08-29 17:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (47.60%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;          // last element of nums1
+        int j = n - 1;          // last element of nums2
+
+        for (int k = m + n - 1; k >= 0; k--) {
+
+            if (j < 0) {
+                break;
+            }
+
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            
+        } }
+    }
+}
