222caa8517464e9f22ece5341d6d6ebe533f8aff
nitin25J <nitin.25bai11122@vitbhopal.ac.in>
2026-09-02 16:18:26 +0530

    Time: 2 ms (29.49%) | Memory: 47.4 MB (38.74%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            // Farthest position reachable from current range
+            farthest = Math.max(farthest, i + nums[i]);
+
+            // We have reached the end of the current jump
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
