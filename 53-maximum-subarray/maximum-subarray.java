a5298bcf97c11b64ee078282e03a5e73f7e3de23
NITIN SHARMA <nitin.25bai11122@vitbhopal.ac.in>
2026-09-06 19:19:40 +0530

    Time: 1 ms (99.94%) | Memory: 77.4 MB (32.46%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,40 +1,19 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return divide(nums, 0, nums.length - 1);
-    }
 
-    private int divide(int[] nums, int left, int right) {
+        int prefixSum = 0;
+        int minPrefix = 0;
+        int maxSum = Integer.MIN_VALUE;
 
-        if (left == right) {
-            return nums[left];
+        for (int num : nums) {
+
+            prefixSum += num;
+
+            maxSum = Math.max(maxSum, prefixSum - minPrefix);
+
+            minPrefix = Math.min(minPrefix, prefixSum);
         }
 
-        int mid = left + (right - left) / 2;
-
-        int leftSum = divide(nums, left, mid);
-        int rightSum = divide(nums, mid + 1, right);
-
-        int leftMax = Integer.MIN_VALUE;
-        int sum = 0;
-
-        for (int i = mid; i >= left; i--) {
-            sum += nums[i];
-            leftMax = Math.max(leftMax, sum);
-        }
-
-        int rightMax = Integer.MIN_VALUE;
-        sum = 0;
-
-        for (int i = mid + 1; i <= right; i++) {
-            sum += nums[i];
-            rightMax = Math.max(rightMax, sum);
-        }
-
-        int crossSum = leftMax + rightMax;
-
-        return Math.max(
-            Math.max(leftSum, rightSum),
-            crossSum
-        );
+        return maxSum;
     }
 }
